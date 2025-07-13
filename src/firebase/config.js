cc08960615e3bc7bb66a385a078c3bf3ee9ccea8
So Jaehong <woghd4545@email.com>
2025-07-13 23:50:57 +0900

    init

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-}
+  apiKey: "AIzaSyBPLSESa0QKb55FwSoVz2lgRIOfRX-zJjk",
+  authDomain: "ac-work-ad274.firebaseapp.com",
+  projectId: "ac-work-ad274",
+  storageBucket: "ac-work-ad274.firebasestorage.app",
+  messagingSenderId: "486151268585",
+  appId: "1:486151268585:web:f27e8ea056e10693a62878",
+  measurementId: "G-EL9R37GVPF"
+};
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
